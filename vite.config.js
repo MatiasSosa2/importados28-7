1e29d323c8997aaabfec06267efe74c6383226b1
Matias <hola111@gmail.com>
2025-08-15 19:42:17 -0300

    Configure GitHub Pages deployment
     Added GitHub Pages support:
    - Updated vite.config.js with base path '/importados28-7/'
    - Created GitHub Actions workflow for automatic deployment
    - Added gh-pages dependency for manual deployment
    - Build optimized for GitHub Pages hosting
    
     Landing page will be available at:
    https://matiassosa2.github.io/importados28-7/

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/importados28-7/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
